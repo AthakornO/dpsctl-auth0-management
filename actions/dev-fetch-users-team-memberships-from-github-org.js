d86223f45546b81ae1de1dae0cc4036739e9bd10
Athakorn Ongsiriporn <athakorn.o@thoughtworks.com>
2024-02-08 13:42:37 +0700

    chore: change snake case to camel case and use axios

--- actions/dev-fetch-users-team-memberships-from-github-org.js
+++ actions/dev-fetch-users-team-memberships-from-github-org.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const axios = require("axios");
 
 exports.onExecutePostLogin = async (event, api) => {
   /**
@@ -6,74 +6,85 @@ exports.onExecutePostLogin = async (event, api) => {
    */
 
   var body = {
-    client_id: event.secrets.client_id,
-    client_secret: event.secrets.client_secret,
+    clientId: event.secrets.clientId,
+    clientSecret: event.secrets.clientSecret,
     audience: `https://${event.tenant.id}.us.auth0.com/api/v2/`,
-    grant_type: "client_credentials"
+    grantType: "client_credentials",
   };
 
-  var options = { 
-    method: 'POST',
-    url: 'https://' + event.tenant.id + '.us.auth0.com/oauth/token',
-    headers: { 'content-type': 'application/json' },
-    body: JSON.stringify(body)
+  let options = {
+    method: "POST",
+    url: "https://" + event.tenant.id + ".us.auth0.com/oauth/token",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(body),
   };
 
-  request(options, function (error, response, data) {
-    if (error) {
-      throw new Error(error);
-    }
+  axios(options)
+    .then((response) => {
+      let obj = JSON.parse(response);
+      var mgntAccessToken = obj.access_token;
 
-    let obj = JSON.parse(data);
-    var mgnt_access_token = obj.access_token
-
-    let options = {
-      method: 'get',
-      maxBodyLength: Infinity,
-      url: 'https://dev-team12-1.us.auth0.com/api/v2/users/' + event.user.user_id,
-      headers: { 
-        'Accept': 'application/json', 
-        'Authorization': 'Bearer ' + mgnt_access_token
-      }
-    };
-
-    request(options, (error, response, data) => {
-      if (error) {
-        throw new Error(error);
-      }
-
-      let obj = JSON.parse(data);
-
-      var idp_access_token = obj.identities[0].access_token
-      
-      request.get({
-        url: "https://api.github.com/user/teams",
+      let options = {
+        method: "GET",
+        maxBodyLength: Infinity,
+        url:
+          "https://dev-team12-1.us.auth0.com/api/v2/users/" +
+          event.user.user_id,
         headers: {
-          // use token authorization to talk to github API
-          "Authorization": "token " + idp_access_token,
-          // Remember the Application name registered in github?
-          // use it to set User-Agent or request will fail
-          "User-Agent": "dev-dpsctl",
-        }
-      }, function(err, res, data) {
-        event.user.err = err;
-        if (data) {
-          // extract github team names to array
-          var github_teams = JSON.parse(data).map(function(team) {
-            return team.organization.login + "/" + team.slug;
-          });
+          Accept: "application/json",
+          Authorization: "Bearer " + mgntAccessToken,
+        },
+      };
 
-          // deny access if not a member of any org teams
-          if (github_teams.length === 0) return api.access.deny("Access denined")
+      axios(options)
+        .then((response) => {
+          let obj = JSON.parse(response.data);
+          var idpAccessToken = obj.identities[0].access_token;
 
-          let roles_set = new Set(event.user.app_metadata.roles)
+          let options = {
+            method: "GET",
+            url: "https://api.github.com/user/teams",
+            headers: {
+              // use token authorization to talk to github API
+              Authorization: "token " + idpAccessToken,
+              // Remember the Application name registered in github?
+              // use it to set User-Agent or request will fail
+              "User-Agent": "dev-dpsctl",
+            },
+          };
+          axios(options)
+            .then((response) => {
+              const data = response.data;
+              if (data) {
+                // extract github team names to array
+                var githubTeams = JSON.parse(data).map(function (team) {
+                  return team.organization.login + "/" + team.slug;
+                });
 
-          github_teams.forEach((element) => roles_set.add(element));
+                // deny access if not a member of any org teams
+                if (githubTeams.length === 0)
+                  return api.access.deny("Access denined");
 
-          // add teams to the application metadata
-          api.user.setAppMetadata("roles", roles_set);
-        }
-      });
+                let rolesSet = new Set(event.user.app_metadata.roles);
+
+                githubTeams.forEach((element) => rolesSet.add(element));
+
+                // add teams to the application metadata
+                api.user.setAppMetadata("roles", rolesSet);
+              }
+            })
+            .catch((error) => {
+              console.log("3");
+              throw new Error(error);
+            });
+        })
+        .catch((error) => {
+          console.log("2");
+          throw new Error(error);
+        });
+    })
+    .catch((error) => {
+      console.log("1");
+      throw new Error(error);
     });
-  });
 };
